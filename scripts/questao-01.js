57958889fb44fbdcc6b34cd79b80c0d643881b6d
nicolassilva00 <nicolas.m.silva19@aluno.senai.br>
2025-05-19 09:06:55 -0300

    feat: finaliza resolução da questão 01

--- scripts/questao-01.js
+++ scripts/questao-01.js
@@ -1,1 +1,18 @@
- 
+const idades = [17, 21, 16, 25, 19];
+let maioresDeIdade = 0;
+let menoresDeIdade = 0;
+
+for (let i = 0;i < idades.length;i ++) {
+    //condição para contadores de maior e menor idade
+    if (idades[i] >= 18) {
+      maioresDeIdade++;
+    }
+}
+menoresDeIdade = idades.length - maioresDeIdade;
+
+console.log(`a quantidade de pessoas maiores de idade é: ${maioresDeIdade
+
+}`);
+console.log(`a quantidade de pessoas menores de idade é: ${menoresDeIdade}`);
+
+
